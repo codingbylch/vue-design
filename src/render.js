ac151dd22affe65f2e8b45a4d556cd8e472e7049
李陈辉 <lichenhui8@jd.com>
2021-06-23 11:27:33 +0800

    feat(render): vue2所采用的diff算法

--- src/render.js
+++ src/render.js
@@ -402,44 +402,86 @@ function patchChildren(
         default:
           // 核心：Diff算法，新旧节点的子节点都是多个子节点时
           // 若采用将旧节点全移除，新节点全添加，就没有复用可言
-          let lastIndex = 0;
-          for (let i = 0; i < nextChildren.length; i++) {
-            const nextVNode = nextChildren[i];
-            let find = false;
-            for (let j = 0; j < prevChildren.length; j++) {
-              const prevVNode = prevChildren[j];
-              if (nextVNode.key === prevVNode.key) {
-                find = true;
-                patch(prevVNode, nextVNode, container);
-                if (j < lastIndex) {
-                  // 移动
-                  const refNode = nextChildren[i - 1].el.nextSibling;
-                  container.insertBefore(prevVNode.el, refNode);
-                } else {
-                  lastIndex = j;
-                }
-                break;
+          // vue2采用的diff算法，借鉴于开源项目snabbdom，最早采用双端算法的库时citojs
+          let oldStartIdx = 0;
+          let oldEndIdx = prevChildren.length - 1;
+          let newStartIdx = 0;
+          let newEndIdx = nextChildren.length - 1;
+          let oldStartVNode = prevChildren[oldStartIdx];
+          let oldEndVNode = prevChildren[oldEndIdx];
+          let newStartVNode = nextChildren[newStartIdx];
+          let newEndVNode = nextChildren[newEndIdx];
+          while (oldStartIdx <= oldEndIdx && newStartIdx <= newEndIdx) {
+            if (!oldStartVNode) {
+              oldStartVNode = prevChildren[++oldStartIdx];
+            } else if (!oldEndVNode) {
+              oldEndVNode = prevChildren[--oldEndIdx];
+            } else if (oldStartVNode.key === newStartVNode.key) {
+              // 步骤一：oldStartVNode 和 newStartVNode 比对
+              // 调用 patch 函数更新
+              patch(oldStartVNode, newStartVNode, container);
+              // 更新索引，指向下一个位置
+              oldStartVNode = prevChildren[++oldStartIdx];
+              newStartVNode = nextChildren[++newStartIdx];
+            } else if (oldEndVNode.key === newEndVNode.key) {
+              // 步骤二：oldEndVNode 和 newEndVNode 比对
+              // 调用 patch 函数更新
+              patch(oldEndVNode, newEndVNode, container);
+              // 更新索引，指向下一个位置
+              oldEndVNode = prevChildren[--oldEndIdx];
+              newEndVNode = nextChildren[--newEndIdx];
+            } else if (oldStartVNode.key === newEndVNode.key) {
+              // 步骤三：oldStartVNode 和 newEndVNode 比对
+              // 调用 patch 函数更新
+              patch(oldStartVNode, newEndVNode, container);
+              // 将 oldStartVNode.el 移动到 oldEndVNode.el 的后面，也就是 oldEndVNode.el.nextSibling 的前面
+              container.insertBefore(
+                oldStartVNode.el,
+                oldEndVNode.el.nextSibling
+              );
+              // 更新索引，指向下一个位置
+              oldStartVNode = prevChildren[++oldStartIdx];
+              newEndVNode = nextChildren[--newEndIdx];
+            } else if (oldEndVNode.key === newStartVNode.key) {
+              // 步骤四：oldEndVNode 和 newStartVNode 比对
+              // 先调用 patch 函数完成更新
+              patch(oldEndVNode, newStartVNode, container);
+              // 更新完成后，将容器中最后一个子节点移动到最前面，使其成为第一个子节点
+              container.insertBefore(oldEndVNode.el, oldStartVNode.el);
+              // 更新索引，指向下一个位置
+              oldEndVNode = prevChildren[--oldEndIdx];
+              newStartVNode = nextChildren[++newStartIdx];
+            } else {
+              // 遍历旧 children，试图寻找与 newStartVNode 拥有相同 key 值的元素
+              const idxInOld = prevChildren.findIndex(
+                (node) => node.key === newStartVNode.key
+              );
+              if (idxInOld >= 0) {
+                // vnodeToMove 就是在旧 children 中找到的节点，该节点所对应的真实 DOM 应该被移动到最前面
+                const vnodeToMove = prevChildren[idxInOld];
+                // 调用 patch 函数完成更新
+                patch(vnodeToMove, newStartVNode, container);
+                // 把 vnodeToMove.el 移动到最前面，即 oldStartVNode.el 的前面
+                container.insertBefore(vnodeToMove.el, oldStartVNode.el);
+                // 由于旧 children 中该位置的节点所对应的真实 DOM 已经被移动，所以将其设置为 undefined
+                prevChildren[idxInOld] = undefined;
+              } else {
+                mount(newStartVNode, container, false, oldStartVNode.el);
               }
-            }
-            if (!find) {
-              // 挂载新节点
-              // 找到refNode
-              const refNode =
-                i - 1 < 0
-                  ? prevChildren[0].el
-                  : nextChildren[i - 1].el.nextSibling;
-              mount(nextVNode, container, false, refNode);
+              // 将 newStartIdx 下移一位
+              newStartVNode = nextChildren[++newStartIdx];
             }
           }
 
-          // 移除已经不存在的节点
-          for (let i = 0; i < prevChildren.length; i++) {
-            const prevVNode = prevChildren[i];
-            const has = nextChildren.find(
-              (nextVNode) => nextVNode.key === prevVNode.key
-            );
-            if (!has) {
-              container.removeChild(prevVNode.el);
+          if (oldEndIdx < oldStartIdx) {
+            // 添加新节点
+            for (let i = newStartIdx; i <= newEndIdx; i++) {
+              mount(nextChildren[i], container, false, oldStartVNode.el);
+            }
+          } else if (newEndIdx < newStartIdx) {
+            // 移除操作
+            for (let i = oldStartIdx; i <= oldEndIdx; i++) {
+              container.removeChild(prevChildren[i].el);
             }
           }
 
